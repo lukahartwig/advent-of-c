b70698036e7ca8740e7f971dd079c8e029ee1b54
Luka Hartwig <luka.hartwig@schuettflix.com>
2022-12-02 10:16:55 +0100

    Solving 2022/02

---
+++ 2022/02/part1.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+
+    char a, b;
+    int sum = 0;
+
+    fp = fopen("input.txt", "r");
+
+    if (fp == NULL)
+    {
+        return 1;
+    }
+
+    while (getline(&line, &len, fp) != -1)
+    {
+        sscanf(line, "%c %c", &a, &b);
+
+        switch (b)
+        {
+        case 'X':
+            sum += 1;
+            switch (a)
+            {
+            case 'A':
+                sum += 3;
+                break;
+            case 'B':
+                sum += 0;
+                break;
+            case 'C':
+                sum += 6;
+                break;
+            }
+            break;
+        case 'Y':
+            sum += 2;
+            switch (a)
+            {
+            case 'A':
+                sum += 6;
+                break;
+            case 'B':
+                sum += 3;
+                break;
+            case 'C':
+                sum += 0;
+                break;
+            }
+            break;
+        case 'Z':
+            sum += 3;
+            switch (a)
+            {
+            case 'A':
+                sum += 0;
+                break;
+            case 'B':
+                sum += 6;
+                break;
+            case 'C':
+                sum += 3;
+                break;
+            }
+            break;
+        }
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
---
+++ 2022/02/part2.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+
+    char a, b;
+    int sum = 0;
+
+    fp = fopen("input.txt", "r");
+
+    if (fp == NULL)
+    {
+        return 1;
+    }
+
+    while (getline(&line, &len, fp) != -1)
+    {
+        sscanf(line, "%c %c", &a, &b);
+
+        switch (b)
+        {
+        case 'X':
+            sum += 0;
+            switch (a)
+            {
+            case 'A':
+                sum += 3;
+                break;
+            case 'B':
+                sum += 1;
+                break;
+            case 'C':
+                sum += 2;
+                break;
+            }
+            break;
+        case 'Y':
+            sum += 3;
+            switch (a)
+            {
+            case 'A':
+                sum += 1;
+                break;
+            case 'B':
+                sum += 2;
+                break;
+            case 'C':
+                sum += 3;
+                break;
+            }
+            break;
+        case 'Z':
+            sum += 6;
+            switch (a)
+            {
+            case 'A':
+                sum += 2;
+                break;
+            case 'B':
+                sum += 3;
+                break;
+            case 'C':
+                sum += 1;
+                break;
+            }
+            break;
+        }
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
