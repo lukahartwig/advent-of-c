e697225aacbd0e204bc118a55970538c2fd418b2
Luka Hartwig <luka.hartwig@schuettflix.com>
2022-12-04 11:27:47 +0100

    Solving 2022/04

---
+++ 2022/04/part1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int a, b, c, d, count = 0;
+
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+
+    fp = fopen("input.txt", "r");
+
+    if (fp == NULL)
+    {
+        return 1;
+    }
+
+    while (getline(&line, &len, fp) != -1)
+    {
+        sscanf(line, "%d-%d,%d-%d", &a, &b, &c, &d);
+
+        if (a >= c && b <= d || a <= c && b >= d)
+        {
+            count++;
+        }
+    }
+
+    printf("%d\n", count);
+
+    return 0;
+}
